a230d585a79c1487827cadb26cbdc22bb74cf6e8
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-08 15:01:07 +0200

    Add → added vue, axios and bootstrap to project

--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Dischi JSON</title>
+    <title>PHP Dischi JSON</title>    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 </head>
+
+
 <body>
-    
+    <div id="app">
+        {{ message }}
+    </div>
+
+
+
+
+
+    <script src="./js/app.js"></script>
 </body>
 </html>
