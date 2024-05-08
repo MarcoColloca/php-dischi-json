b981b089042a80205f2fee12f9981257604e7d14
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-08 16:47:06 +0200

    Refactor → added comments to methods in app.js

--- js/app.js
+++ js/app.js
@@ -21,38 +21,38 @@ createApp({
 
 
     methods:{
+        /* 
+            Funzione che recupera i dati effettuando una chiamata GET al file server.php,
+            Quindi va a modificare il data dischi assegnandogli il valore results dentro il valore data della risposta ricevuta.
+        */
         fetchData(){
             axios.get('./server.php').then((res) => {
-				//console.log(res.data.results)
 				this.dischi = res.data.results
-                //console.log(this.dischi)
 			})
         },
 
+        /* 
+            Funzione che passato un indice come parametro, va a modificare il data objectIndex (rendendolo uguale al parametro); 
+            quindi esegue un ciclo for dell'Array dischi, andando ad assegnare all'oggetto modalDisk il valore dell'oggetto contenuto dentro dischi all'indice corrispodnente al parametro 
+        */
         openModal(index){
-            //console.log('funziona');
+
             this.isOpenModal = true;
 
             this.objectIndex = index;
-            //console.log(this.objectIndex)
-
-            
-            
-            //console.log(this.dischi)
 
             for(let i = 0; i < this.dischi.length; i++){
-                // console.log(i)
-                // console.log( this.dischi[i])
 
                 if(i === this.objectIndex){
                     this.modalDisk = this.dischi[i]
                 }
-
-                
             }
-           
         },
 
+
+        /*
+            Funzione che cambia il valore isOpenModal in false, utilizzata per chiudere il modale grazie alla direttiva v-show presente in index.php
+        */
         closeModal(){
             this.isOpenModal = false;
         }
