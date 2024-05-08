bc5c32651a587bd31694d788125f83b9eaf99c34
MarcoColloca <marco.colloca3511@gmail.com>
2024-05-08 16:40:00 +0200

    Feat → added modal on click that shows disk infos

--- index.php
+++ index.php
@@ -25,8 +25,8 @@
             <section class="disk-cards py-5">
                 <div class="container">
                     <div class="row g-5">
-                        <div class="col-4" v-for="disco in dischi">
-                            <div class="card text-bg-special p-5">
+                        <div class="col-4" v-for="(disco, i) in dischi">
+                            <div class="card text-bg-special p-5" @click="openModal(i)">
                                 <img :src="disco.poster" class="card-img-top" alt="...">
                                 <div class="card-body">
                                     <h5 class="card-title text-center">{{disco.title}}</h5>
@@ -37,7 +37,22 @@
                         </div>
                     </div>
                 </div>
-            </section>
+                <span class="my-modal" v-show="isOpenModal">
+                    <div class="modal-card-container">
+                        <div class="card text-bg-special p-5">
+                            <img :src="modalDisk.poster" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">{{modalDisk.title}}</h5>
+                                <p class="card-text text-center">{{modalDisk.author}}</p>
+                                <h5 class="card-title text-center">{{modalDisk.year}}</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="close-modal" @click="closeModal">
+                        X
+                    </p>
+                </span>
+            </section>            
         </main>
         
        
@@ -49,6 +64,16 @@
 
 
 <style>
+
+    /** Utilities */
+
+    .my-d-none{
+        display: none;
+    }
+
+    .my-d-block{
+        display: block;
+    }
 
     /** Style */
     .text-bg-special{
@@ -71,6 +96,35 @@
     /** Main */
     main{
         background-color: #1D2D3C;
+    }
+    
+    img{
+        max-width: 300px;
+    }
+
+    .my-modal{        
+        position: fixed;
+        top: 30px;
+        left: 30px;
+        width: 95%;
+        height: 95%;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    .modal-card-container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+
+    .close-modal{
+        color: white;
+        font-size: 40px;
+        position: absolute;
+        top: 20px;
+        right: 40px;
+        cursor: pointer;
     }
 </style>
 </html>
--- js/app.js
+++ js/app.js
@@ -6,23 +6,55 @@ createApp({
         return {
             title: 'PHP Dischi JSON!',
             dischi: [],
+
+            objectIndex: 0,
+            modalDisk: {},
+
+            isOpenModal: false,
         }
     },
 
 
     created(){
         this.fetchData();
-
-        
     },
+
 
     methods:{
         fetchData(){
             axios.get('./server.php').then((res) => {
 				//console.log(res.data.results)
 				this.dischi = res.data.results
-                console.log(this.dischi)
+                //console.log(this.dischi)
 			})
+        },
+
+        openModal(index){
+            //console.log('funziona');
+            this.isOpenModal = true;
+
+            this.objectIndex = index;
+            //console.log(this.objectIndex)
+
+            
+            
+            //console.log(this.dischi)
+
+            for(let i = 0; i < this.dischi.length; i++){
+                // console.log(i)
+                // console.log( this.dischi[i])
+
+                if(i === this.objectIndex){
+                    this.modalDisk = this.dischi[i]
+                }
+
+                
+            }
+           
+        },
+
+        closeModal(){
+            this.isOpenModal = false;
         }
     }
 }).mount('#app')
